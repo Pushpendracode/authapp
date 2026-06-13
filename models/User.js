92abaeb928b394bb482bcaad8d1afaaa72a78e08
Pushpendracode <pushpendra226664@gmail.com>
2026-06-13 14:04:00 +0530

    Fix User model pre save hook

--- models/User.js
+++ models/User.js
@@ -30,13 +30,9 @@ const userSchema = new mongoose.Schema(
 
 // Hash password before saving
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-    return;
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
 });
 
 // Method to compare passwords
