c5c5de6af6c2019033951d607253ea17c695af6e
Pushpendracode <pushpendra226664@gmail.com>
2026-06-13 15:04:27 +0530

    Debug env vars

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log('JWT_SECRET:', process.env.JWT_SECRET ? 'LOADED' : 'MISSING');
+console.log('MONGODB_URI:', process.env.MONGODB_URI ? 'LOADED' : 'MISSING');
+
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
